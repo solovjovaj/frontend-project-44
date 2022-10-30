d01565f3da6f3c0a171cf360e952e963cb74e813
Yulia Solovyeva <solovjovaj@seznam.cz>
2022-10-30 17:17:54 +0100

    fixed prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,10 +6,10 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
 const primeGame = () => {
