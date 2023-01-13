25a69c90a04b69c259276305a3b0259913b8d924
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-13 15:27:09 +0300

    linter fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line import/no-unresolved, import/extensions
+
 import getRandomInt from '../getRandomNumber.js';
-// eslint-disable-next-line import/no-unresolved, import/extensions
 import gameLogic from '../index.js';
 
 const generateProgression = (startStep, step, progresLength) => {
