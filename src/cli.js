c2b7f83666a21bb4913fabea7bf6fb870d54e6a5
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-13 15:06:18 +0300

    delete consolelog

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,7 @@ const askName = () => {
   // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  return `Hello, ${userName}!`;
+  // eslint-disable-next-line no-console
+  return console.log(`Hello, ${userName}!`);
 };
 export default askName;
