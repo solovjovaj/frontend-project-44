472c516d79c7586c74b90aa28f6e4feac10ac022
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-13 15:24:38 +0300

    fix

--- src/games/even.js
+++ src/games/even.js
@@ -5,9 +5,8 @@ const isEven = (num) => num % 2 === 0;
 const rule = 'Answer "yes" if the number is even, other answer "no"';
 const findEven = () => {
   const randomNum = getRandomInt(1, 100);
-  const question = `${randomNum}`;
   const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
-  return [question, correctAnswer];
+  return [`${randomNum}`, correctAnswer];
 };
 
 export default () => gameLogic(rule, findEven);
