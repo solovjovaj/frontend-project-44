07bce134922ff622234b7267c6dcee6f17ecf190
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-13 14:58:11 +0300

    changed evengame

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,11 @@ import gameLogic from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
 const rule = 'Answer "yes" if the number is even, other answer "no"';
-const gameEven = () => {
+const findEven = () => {
   const randomNum = getRandomInt(1, 100);
   const question = `${randomNum}`;
   const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
-export default () => gameLogic(rule, gameEven);
+export default () => gameLogic(rule, findEven);
