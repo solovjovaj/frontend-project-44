b9444b5c781e92670607b18895e7f6a6e7e4fcaf
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-18 13:28:37 +0300

    fix linter

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-// eslint-disable-next-line import/prefer-default-export
 const askName = () => {
   // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
