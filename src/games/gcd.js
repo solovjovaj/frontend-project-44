561e236ca0eb51afc2ec8407995fc0d053856665
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-18 13:41:21 +0300

    magic number

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,8 +5,8 @@ import gameLogic from '../index.js';
 const rule = 'Find the greatest common divisor of given numbers.';
 const getGcd = (a, b) => (b ? getGcd(b, a % b) : a);
 const findGcd = () => {
-  const num1 = getRandomInt(0, 100);
-  const num2 = getRandomInt(0, 100);
+  const num1 = getRandomInt();
+  const num2 = getRandomInt();
   const question = `${num1} ${num2}`;
   const correctAnswer = getGcd(num1, num2).toString();
   return [question, correctAnswer];
