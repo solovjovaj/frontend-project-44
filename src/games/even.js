f3cce0924b6e6cf75e080b7faa0a99e17eff4b15
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-18 13:39:11 +0300

    magic number

--- src/games/even.js
+++ src/games/even.js
@@ -3,8 +3,10 @@ import gameLogic from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
 const rule = 'Answer "yes" if the number is even, other answer "no"';
+const minNumber = 1;
+const maxnumber = 100;
 const findEven = () => {
-  const randomNum = getRandomInt(1, 100);
+  const randomNum = getRandomInt(minNumber, maxnumber);
   const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
   return [`${randomNum}`, correctAnswer];
 };
