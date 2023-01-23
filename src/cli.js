8947455ac83823668b3d2df62b44b49f4d7aa84e
Yulia Solovyeva <solovjovaj@seznam.cz>
2023-01-23 11:21:32 +0300

    f

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
-  // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  // eslint-disable-next-line no-console
   return console.log(`Hello, ${userName}!`);
 };
 export default askName;
